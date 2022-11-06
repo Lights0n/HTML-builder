f615ba09658d2fb6bd2176b5c878d56ea21643b6
Lights0n <lyadouskiy@gmail.com>
2022-11-07 00:15:54 +0300

    feat: implement 04 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const mkdir = require('node:fs/promises')
+const { copyFile, readdir } = require('node:fs/promises')
 function copyDir() {
 
   async function makeDirectory() {
@@ -9,7 +10,6 @@ function copyDir() {
     const dirCreation = await fs.mkdir(projectFolder, { recursive: true }, (err) => {
 
       if (err) { console.log(err); }
-
     });
   }
   makeDirectory().catch(console.error);
@@ -19,28 +19,15 @@ function copyDir() {
     if (err) {
       console.log(err);
     }
-
     directory.forEach(file => {
-      console.log(file.name);
-      console.log(path.join(__dirname, 'files-copy'));
-      // TODO its all about true pathes
-      // https://www.itsolutionstuff.com/post/how-to-copy-file-to-another-directory-in-node-jsexample.html
-      try {
-        fs.copyFile(path.join(__dirname, file.name.toString()), path.join(__dirname, 'files-copy'), (err) => {
-          if (err) {
-            throw err
-          }
-        })
-        console.log('SUCCESS');
-      }
-      catch {
-        console.log('The file could not be copied');
-      }
-      // fs.mkdir(file.name, path.join(__dirname, 'files-copy'), (err) => { });
-      /* if (file.isFile()) {
-        console.log(file.name);
-      } */
 
+
+
+      fs.copyFile(path.join(__dirname, 'files',file.name), path.join(__dirname, 'files-copy', file.name), (err) => {
+        if (err) {
+          console.log("Oops! An Error Occured:", err);
+        }
+      });
     })
   })
 
